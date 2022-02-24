abca2d101eed7f922386c04df18b792fa47d8303
Егрищин Матвей <matvey20158@mail.ru>
2022-02-24 22:20:26 +0300

    demo

--- templates/articles/view.php
+++
@@ -1,9 +0,0 @@
-<?php include __DIR__ . '/../header.php'; ?>
-    <h1><?= $article->getNameFilm() ?></h1>
-    <p><?= $article->getText() ?></p>
-
-
-<?php include __DIR__ . '/../footer.php';
-
-
-?>
